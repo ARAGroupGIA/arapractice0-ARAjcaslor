50ca783a4cee59c5b9147702f4b40dca061d4250
ARAjcaslor <jcaslor@etsid.upv.es>
2019-09-11 14:16:51 +0200

    Version 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,12 @@
 /* Here, you should entry the required libraries */
+/* sumar 2 numeros primero version 2 3 numeros */
+#include<stdio.h>
 
 void main(){
-	/* Here, you should write the source code of your program */
+	int a,b,c;
+	printf("introduzca un numero:");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	c=a+b;
+	printf("la suma de %d y %d es %d",a,b,c);
 }
